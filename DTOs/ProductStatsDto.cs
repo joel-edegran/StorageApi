525f79b47dd2f667d845980641dd57966b8a50d4
Joel Edegran <joel_edegran@hotmail.com>
2026-06-16 16:00:17 +0200

    Add products stats endpoint

---
+++ DTOs/ProductStatsDto.cs
@@ -0,0 +1,8 @@
+﻿namespace StorageApi.DTOs;
+
+public class ProductStatsDto
+{
+    public int TotalProducts { get; set; }
+    public int TotalInventoryValue { get; set; }
+    public double AveragePrice { get; set; }
+}
